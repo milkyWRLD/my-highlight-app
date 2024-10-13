fba18792117d5b8d00d093a5fb01cb19ac48ecc3
artsid <heavydisto@gmail.com>
2024-10-13 21:48:47 +0300

    Complete!

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,128 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function New(props: { children: React.ReactNode }) {
+	return (
+		<div className='wrap-item wrap-item-new'>
+			<span className='label'>New!</span>
+			{props.children}
+		</div>
+	)
 }
 
-export default App
+function Popular(props: { children: React.ReactNode }) {
+	return (
+		<div className='wrap-item wrap-item-popular'>
+			<span className='label'>Popular!</span>
+			{props.children}
+		</div>
+	)
+}
+
+interface WithViews {
+	views: number
+}
+
+function withPopularOrNew<T extends WithViews>(
+	Component: React.ComponentType<T>
+) {
+	return (props: T) => {
+		const { views } = props
+
+		if (views >= 1000) {
+			return <Popular>{<Component {...props} />}</Popular>
+		} else if (views < 100) {
+			return <New>{<Component {...props} />}</New>
+		} else {
+			return <Component {...props} />
+		}
+	}
+}
+
+const WrappedVideo = withPopularOrNew(Video)
+const WrappedArticle = withPopularOrNew(Article)
+
+interface ArticleProps {
+	type: string
+	title: string
+	views: number
+}
+
+function Article(props: ArticleProps) {
+	return (
+		<div className='item item-article'>
+			<h3>
+				<a href='#'>{props.title}</a>
+			</h3>
+			<p className='views'>Прочтений: {props.views}</p>
+		</div>
+	)
+}
+
+interface VideoProps {
+	type: string
+	url: string
+	views: number
+}
+
+function Video(props: VideoProps) {
+	return (
+		<div className='item item-video'>
+			<iframe
+				src={props.url}
+				frameBorder='0'
+				allow='autoplay; encrypted-media'
+				allowFullScreen
+			></iframe>
+			<p className='views'>Просмотров: {props.views}</p>
+		</div>
+	)
+}
+
+function List(props: { list: (ArticleProps | VideoProps)[] }) {
+	return props.list.map((item, index) => {
+		switch (item.type) {
+			case 'video':
+				return <WrappedVideo key={index} {...(item as VideoProps)} />
+
+			case 'article':
+				return <WrappedArticle key={index} {...(item as ArticleProps)} />
+		}
+	})
+}
+
+export default function App() {
+	const [list, setList] = useState([
+		{
+			type: 'video',
+			url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
+			views: 50,
+		},
+		{
+			type: 'video',
+			url: 'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
+			views: 12,
+		},
+		{
+			type: 'article',
+			title: 'Невероятные события в неизвестном поселке...',
+			views: 175,
+		},
+		{
+			type: 'article',
+			title: 'Секретные данные были раскрыты!',
+			views: 1532,
+		},
+		{
+			type: 'video',
+			url: 'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
+			views: 4253,
+		},
+		{
+			type: 'article',
+			title: 'Кот Бегемот обладает невероятной...',
+			views: 12,
+		},
+	])
+
+	return <List list={list} />
+}
